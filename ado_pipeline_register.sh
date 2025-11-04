d0ca8a82b58d0def2fd582030d92857c943346f3
Ashley Kell <ashleyjkell@gmail.com>
2025-11-04 15:12:46 +0000

    Update Azure DevOps agent version in script

--- ado_pipeline_register.sh
+++ ado_pipeline_register.sh
@@ -5,7 +5,7 @@ sudo chown azureuser /agent
 
 wget https://download.agent.dev.azure.com/agent/4.264.2/vsts-agent-linux-x64-4.264.2.tar.gz
 
-tar zxvf vsts-agent-linux-x64-3.243.0.tar.gz
+tar zxvf vsts-agent-linux-x64-4.264.2.tar.gz
 echo "Reached here"
 su - azureuser -c "cd /agent && /agent/config.sh --unattended --url $1 --auth pat --token $2 --pool default --agent myAgent --acceptTeeEula --work /agent"
 echo "Command complete"
